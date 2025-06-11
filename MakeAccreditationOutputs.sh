e999436102c384b926713cb1a397736c48139de6
Rachel Cardell-Oliver <rachel.cardell-oliver@uwa.edu.au>
2025-06-11 11:27:49 +0800

    Updated scripts
    Updated directory structure, generation scripts and python requirements

--- MakeAccreditationOutputs.sh
+++ MakeAccreditationOutputs.sh
@@ -5,6 +5,10 @@
 source DirectoryNames.sh
 
 # make clean versions of all directories
+rm -f $HTML_OUTPUT/*.html
+rm -f $MSWORD_OUTPUT/*.docx
+
+
 rm -r $OUTPUT_DIR
 mkdir $OUTPUT_DIR
 mkdir $EXCEL_OUTPUT
@@ -13,6 +17,9 @@ mkdir $EXCEL_UNITS_OUTPUT
 mkdir $HTML_OUTPUT
 mkdir $LATEX_OUTPUT
 mkdir $LATEX_TABLES_OUTPUT
+mkdir $MSWORD_OUTPUT
+
+
 
 # Generate latex, excel and html outputs by running all scripts
 
@@ -21,3 +28,7 @@ python3 $KNOWLEDGEBASE_DIR/knowledgebase.py
 bash $LATEX_DIR/generateLatex.sh
 bash $HTML_DIR/generateHTML.sh
 bash $EXCEL_DIR/generateExcel.sh
+bash $MSWORD_DIR/generateMSWord.sh
+
+exit 0
+
